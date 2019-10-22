abaa7c9cbfd6070d40782683aa9515f38ffb31b7
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-10-22 09:48:20 +0300

    Base router

--- eventScheduler/src/app/header/header.component.ts
+++ eventScheduler/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +7,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output() choiceSelected = new EventEmitter<string>();
-
-  onSelected(choice: string) {
-    this.choiceSelected.emit(choice);
-  }
+  
   constructor() { }
 
   ngOnInit() {
