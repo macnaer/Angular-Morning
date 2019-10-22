d9016cffbd4105156857b4e5194a17f57e6dc03b
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2019-10-22 07:25:31 +0300

    Connect event to skills

--- eventScheduler/src/app/events/event.service.ts
+++ eventScheduler/src/app/events/event.service.ts
@@ -1,9 +1,25 @@
 import Event from "../models/event";
+import Skills from '../models/skills.model';
 
 export class EventService{
     private events: Event[] = [
-        new Event("Svelte", "Svelte description", "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/236/full/svelte-logo-vertical.png"),
-        new Event("Angular", "Angular description", "https://user-images.githubusercontent.com/7990614/27530684-09d7c75a-5a5a-11e7-9893-1395507feb2e.png"),
+        new Event(
+            "Svelte",
+            "Svelte description",
+            "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/001/236/full/svelte-logo-vertical.png",
+            [
+                new Skills("Html", "Beginner"),
+                new Skills("C++", "Beginner")
+            ]),
+        new Event(
+            "Angular",
+            "Angular description",
+            "https://user-images.githubusercontent.com/7990614/27530684-09d7c75a-5a5a-11e7-9893-1395507feb2e.png",
+            [
+                new Skills("C#", "Master"),
+                new Skills("WinAPI", "Master"),
+                new Skills("Java", "Master"),
+            ]),
     ];
 
     getAllEvents(){
--- eventScheduler/src/app/models/event.ts
+++ eventScheduler/src/app/models/event.ts
@@ -1,10 +1,14 @@
+import Skills from './skills.model';
+
 export default class Event{
     title: string;
     description: string;
     imgPath: string;
-    constructor(title: string, desc: string, imgPath:string){
+    skills: Skills[];
+    constructor(title: string, desc: string, imgPath:string, skills:Skills[]){
         this.title = title;
         this.description = desc;
         this.imgPath = imgPath;
+        this.skills = skills;
     }
 }
